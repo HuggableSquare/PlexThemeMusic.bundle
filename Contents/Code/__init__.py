d86d342e5181e34abaf9152566390b20a2b86a6c
James Clarke <james@plexapp.com>
2010-08-04 02:00:18 +0100

    Initial commit

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,26 @@
+#Plex Theme Music
+THEME_URL = 'http://tvthemes.plexapp.com/%s.mp3'
+
+def Start():
+  HTTP.CacheTime = CACHE_1DAY
+  
+class PlexThemeMusicAgent(Agent.TV_Shows):
+  name = 'Plex Theme Music'
+  languages = [Locale.Language.English]
+  primary_provider = False
+  contributes_to = ['com.plexapp.agents.thetvdb']
+  
+  def search(self, results, media, lang):
+    results.Append(
+      MetadataSearchResult(
+        id    = media.primary_metadata.id.replace('tt',''),
+        score = 100
+      )    
+    )
+    
+  def update(self, metadata, lang):
+    if THEME_URL % metadata.id not in metadata.themes:
+      try:
+        metadata.themes[THEME_URL % metadata.id] = Proxy.Media(HTTP.Request(THEME_URL % metadata.id))
+      except:
+        pass

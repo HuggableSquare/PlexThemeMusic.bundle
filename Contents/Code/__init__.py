fb5c77a60c925dcfd60e75a945244e07ee009e7c
sander1 <sander@plexapp.com>
2014-07-09 14:21:52 +0200

    Contribute theme music to new Movie Database (TV) Agent.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -8,15 +8,34 @@ class PlexThemeMusicAgent(Agent.TV_Shows):
   name = 'Plex Theme Music'
   languages = [Locale.Language.NoLanguage]
   primary_provider = False
-  contributes_to = ['com.plexapp.agents.thetvdb']
+  contributes_to = [
+    'com.plexapp.agents.thetvdb',
+    'com.plexapp.agents.themoviedb'
+  ]
 
   def search(self, results, media, lang):
-    results.Append(
-      MetadataSearchResult(
-        id    = media.primary_metadata.id,
+
+    if media.primary_agent == 'com.plexapp.agents.thetvdb':
+      results.Append(MetadataSearchResult(
+        id = media.primary_metadata.id,
         score = 100
-      )    
-    )
+      ))
+
+    elif media.primary_agent == 'com.plexapp.agents.themoviedb':
+      # Get the TVDB id from the Movie Database Agent
+      tvdb_id = Core.messaging.call_external_function(
+        'com.plexapp.agents.themoviedb',
+        'MessageKit:GetTvdbId',
+        kwargs = dict(
+          tmdb_id = media.primary_metadata.id
+        )
+      )
+
+      if tvdb_id:
+        results.Append(MetadataSearchResult(
+          id = tvdb_id,
+          score = 100
+        ))
 
   def update(self, metadata, media, lang):
     if THEME_URL % metadata.id not in metadata.themes:

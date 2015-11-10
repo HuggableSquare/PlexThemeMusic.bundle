8c5562372f5d8655ea927bf646feef078bac2450
HuggableSquare <huggablesquare@gmail.com>
2015-11-09 23:01:55 -0500

    initial forked commit

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -5,37 +5,20 @@ def Start():
   HTTP.CacheTime = CACHE_1DAY
   
 class PlexThemeMusicAgent(Agent.TV_Shows):
-  name = 'Plex Theme Music'
+  name = 'Plex Theme Music DVD Order'
   languages = [Locale.Language.NoLanguage]
   primary_provider = False
   contributes_to = [
-    'com.plexapp.agents.thetvdb',
-    'com.plexapp.agents.themoviedb'
+	'com.plexapp.agents.thetvdbdvdorder'
   ]
 
   def search(self, results, media, lang):
 
-    if media.primary_agent == 'com.plexapp.agents.thetvdb':
+    if media.primary_agent == 'com.plexapp.agents.thetvdbdvdorder':
       results.Append(MetadataSearchResult(
         id = media.primary_metadata.id,
         score = 100
       ))
-
-    elif media.primary_agent == 'com.plexapp.agents.themoviedb':
-      # Get the TVDB id from the Movie Database Agent
-      tvdb_id = Core.messaging.call_external_function(
-        'com.plexapp.agents.themoviedb',
-        'MessageKit:GetTvdbId',
-        kwargs = dict(
-          tmdb_id = media.primary_metadata.id
-        )
-      )
-
-      if tvdb_id:
-        results.Append(MetadataSearchResult(
-          id = tvdb_id,
-          score = 100
-        ))
 
   def update(self, metadata, media, lang):
     if THEME_URL % metadata.id not in metadata.themes:
